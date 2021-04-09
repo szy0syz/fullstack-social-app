446b4668590d0111911e50719179340e5ad67023
szy0syz <szy0syz@gmail.com>
2021-04-09 22:55:36 +0800

    feat: Models

---
+++ models/ProfileModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ProfileSchema = new Schema(
+  {
+    user: { type: Schema.Types.ObjectId, ref: 'user' },
+    bio: { type: String, require: true },
+    social: {
+      youtube: { type: String },
+      twitter: { type: String },
+      facebook: { type: String },
+      instagram: { type: String },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('profile', ProfileSchema);
