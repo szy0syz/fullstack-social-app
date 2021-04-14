57f6f9563fd6fae9d275c4a174ab1771122a62a5
szy0syz <szy0syz@gmail.com>
2021-04-14 22:34:03 +0800

    feat: 新增Post四个组件

--- components/Post/PostComments.js
+++ components/Post/PostComments.js
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Comment, Icon } from 'semantic-ui-react';
+import calcTime from '../../utilsClient/calculateTime';
 
-function PostComments() {
-  return <div></div>;
+function PostComments({ comment, user, setComments, postId }) {
+  const [disabled, setDisable] = useState(false);
+
+  return (
+    <>
+      <Comment.Group>
+        <Comment>
+          <Comment.Avatar src={comment.user.profilePicUrl} />
+          <Comment.Content>
+            <Comment.Author as="a" href={`/${comment.user.username}`}>
+              {comment.user.name}
+            </Comment.Author>
+            <Comment.Metadata>{calcTime(comment.date)}</Comment.Metadata>
+
+            <Comment.Text>{comment.text}</Comment.Text>
+
+            <Comment.Actions>
+              <Comment.Action>
+                {(user.role === 'root' || comment.user._id === user._id) && (
+                  <Icon
+                    disabled={disabled}
+                    color="red"
+                    name="trash"
+                    onClick={async () => {
+                      setDisabled(true);
+                      await deleteComment(postId, comment._id, setComments);
+                      setDisabled(false);
+                    }}
+                  />
+                )}
+              </Comment.Action>
+            </Comment.Actions>
+          </Comment.Content>
+        </Comment>
+      </Comment.Group>
+    </>
+  );
 }
 
 export default PostComments;
---
+++ utilsClient/postActions.js
@@ -0,0 +1,15 @@
+export const postComment = async (
+  postId,
+  user,
+  text,
+  setComments,
+  setText
+) => {};
+
+export const submitNewPost = async (
+  postId,
+  user,
+  text,
+  setComments,
+  setText
+) => {};
