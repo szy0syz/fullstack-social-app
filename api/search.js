4a8b1bd97c5fb46a111cdcd2abb6abf4c0938239
szy0syz <szy0syz@gmail.com>
2021-04-16 00:06:07 +0800

    feat: chat 相关功能

--- api/search.js
+++ api/search.js
@@ -1,10 +1,11 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const authMiddleware = require("../middleware/authMiddleware");
-const UserModel = require("../models/UserModel");
+const authMiddleware = require('../middleware/authMiddleware');
+const UserModel = require('../models/UserModel');
 
-router.get("/:searchText", authMiddleware, async (req, res) => {
+router.get('/:searchText', authMiddleware, async (req, res) => {
   try {
+    const { userId } = req;
     const { searchText } = req.params;
 
     if (searchText.length === 0) return;
@@ -12,10 +13,14 @@ router.get("/:searchText", authMiddleware, async (req, res) => {
     let userPattern = new RegExp(`^${searchText}`);
 
     const results = await UserModel.find({
-      name: { $regex: userPattern, $options: "i" }
+      name: { $regex: userPattern, $options: 'i' },
     });
 
-    return res.status(200).json(results);
+    const resultsToBeSent =
+      results.length > 0 &&
+      results.filter((result) => result._id.toString() !== userId);
+
+    return res.status(200).json(resultsToBeSent);
   } catch (error) {
     console.error(error);
     return res.status(500).send(`Server error`);
--- pages/messages.js
+++ pages/messages.js
@@ -1,13 +1,78 @@
 import axios from 'axios';
 import { parseCookies } from 'nookies';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import baseUrl from '../utils/baseUrl';
-import { Segment, Header, Divider, Comment, Grid, Icon } from 'semantic-ui-react';
+import {
+  Segment,
+  Header,
+  Divider,
+  Comment,
+  Grid,
+  Icon,
+} from 'semantic-ui-react';
+import Chat from '../components/Chats/Chat';
+import ChatListSearch from '../components/Chats/ChatListSearch';
+import { useRouter } from 'next/router';
+import { NoMessages } from '../components/Layout/NoData';
 
 function Messages({ chatsData }) {
   const [chats, setChats] = useState(chatsData);
+  const router = useRouter();
 
-  return <div>{JSON.stringify(chats)}</div>;
+  useEffect(() => {
+    if (chats.length > 0 && !router.query.message) {
+      router.push(`/messages?message=${chats[0].messagesWith}`, undefined, {
+        shallow: true,
+      });
+    }
+  }, []);
+
+  return (
+    <>
+      <Segment>
+        <Header
+          icon="home"
+          content="Go Back!"
+          onClick={() => router.push('/')}
+          style={{ cursor: 'pointer' }}
+        />
+
+        <Divider hidden />
+
+        <div style={{ marginBottom: '10px' }}>
+          <ChatListSearch user={user} chats={chats} setChats={setChats} />
+        </div>
+
+        {chats.length > 0 ? (
+          <>
+            <Grid stackable>
+              <Grid.Column width={4}>
+                <Comment.Group size="big">
+                  <Segment
+                    raised
+                    style={{ overflow: 'auto', maxHeight: '32rem' }}
+                  >
+                    {chats.map((chat, i) => (
+                      <Chat
+                        key={i}
+                        chat={chat}
+                        connectedUsers={connectedUsers}
+                        deleteChat={deleteChat}
+                      />
+                    ))}
+                  </Segment>
+                </Comment.Group>
+              </Grid.Column>
+            </Grid>
+          </>
+        ) : (
+          <>
+            <NoMessages />
+          </>
+        )}
+      </Segment>
+    </>
+  );
 }
 
 Messages.getInitialProps = async (ctx) => {
